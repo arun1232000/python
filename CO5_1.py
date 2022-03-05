8156470580e3a6578f9c5bd53209e103cb6e7148
arun1232000 <94525520+arun1232000@users.noreply.github.com>
2022-03-05 12:19:08 +0530

    Create CO5_1.py

---
+++ CO5_1.py
@@ -0,0 +1,8 @@
+"""
+Write a Python program to read a file line by line and store it into a list.
+"""
+
+with open("sample.txt",'r') as file:
+    output=file.readlines()
+
+print("Content of file is          :",output)
